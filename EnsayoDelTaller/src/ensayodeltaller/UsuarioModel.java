987d75e45771c650c417bb76c29a4e858222664e
santanapablo1975 <santanapablo1975@gmail.com>
2016-11-23 11:21:30 -0500

    Add files via upload

---
+++ EnsayoDelTaller/src/ensayodeltaller/UsuarioModel.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ensayodeltaller;
+
+/**
+ *
+ * @author Pc12
+ */
+public class UsuarioModel {
+    private String Nombre;
+    private String Apellido;
+    private int Edad;
+
+    /**
+     * @return the Nombre
+     */
+    public String getNombre() {
+        return Nombre;
+    }
+
+    /**
+     * @param Nombre the Nombre to set
+     */
+    public void setNombre(String Nombre) {
+        this.Nombre = Nombre;
+    }
+
+    /**
+     * @return the Apellido
+     */
+    public String getApellido() {
+        return Apellido;
+    }
+
+    /**
+     * @param Apellido the Apellido to set
+     */
+    public void setApellido(String Apellido) {
+        this.Apellido = Apellido;
+    }
+
+    /**
+     * @return the Edad
+     */
+    public int getEdad() {
+        return Edad;
+    }
+
+    /**
+     * @param Edad the Edad to set
+     */
+    public void setEdad(int Edad) {
+        this.Edad = Edad;
+    }
+    
+    
+}
